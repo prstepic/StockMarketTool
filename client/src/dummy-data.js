7ede9fda7f410641f2e6e899672af85ae56c3f5e
Preston Stepic <prestonstepic@Prestons-Air.attlocal.net>
2021-04-01 23:51:09 -0500

    simple page and dummy data

---
+++ client/src/dummy-data.js
@@ -0,0 +1,48 @@
+export const fakeLastPrice = [
+  {
+    ticker: 'AAPL',
+    lastPrice: '120',
+    date: '4/1/2021'
+  },
+  {
+    ticker: 'GOOGL',
+    lastPrice: '2130',
+    date: '4/1/2021'
+  },
+  {
+    ticker: 'MSFT',
+    lastPrice: '240',
+    date: '4/1/2021'
+  },
+  {
+    ticker: 'BA',
+    lastPrice: '250',
+    date: '4/1/2021'
+  },
+  {
+    ticker: 'AMZN',
+    lastPrice: '3160',
+    date: '4/1/2021'
+  },
+  {
+    ticker: 'NET',
+    lastPrice: '70',
+    date: '4/1/2021'
+  },
+  {
+    ticker: 'GE',
+    lastPrice: '13',
+    date: '4/1/2021'
+  },
+  {
+    ticker: 'ARKK',
+    lastPrice: '120',
+    date: '4/1/2021'
+  },
+  {
+    ticker: 'SQ',
+    lastPrice: '230',
+    date: '4/1/2021'
+  }
+]
+
--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,16 +1,41 @@
 <template>
-  <h1> This is the Home Page </h1>
+
+  <div class="pageView">
+    <h1> Your Stock Dashboard </h1>
+    <div class="tickerGrid">
+      <div v-for="stock in fakeLastPrice" class="stockItem" v-bind:key="stock.ticker">
+        <h3 class="tickerName"> {{ stock.ticker }} </h3>
+        <h5 class="tickerPrice"> ${{ stock.lastPrice }} </h5>
+        <p class="marketDate"> {{ stock.date }} </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+  import { fakeLastPrice } from '../dummy-data'
   export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    data() {
+      return {
+        fakeLastPrice
+      }
+    }
   }
 </script>
 
 <style scoped>
   h1 {
     color: red;
+  }
+  .tickerGrid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 50px;
+    grid-auto-rows: minmax(100px, auto);
+  }
+  .stockItem {
+    border: 1px solid black;
   }
 </style>
 
