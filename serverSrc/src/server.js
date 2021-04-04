9e11a9e76c05951863f9120c58825487e50ac81c
Preston Stepic <prestonstepic@Prestons-Air.attlocal.net>
2021-04-04 17:24:35 -0500

    Simple Server Functionality

--- serverSrc/src/server.js
+++ serverSrc/src/server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const mongodb = require("mongodb")
-import { fakeLasPrices } from '../dummy-data'
+
+import { fakeLastPrices } from '../dummy-data'
 import { indices } from '../dummy-data'
 
 const app = express()
 const port = 9090
+
+app.use(express.json())
 
 app.listen(port, () => {
     console.log('server running on port ' + port)
@@ -14,24 +17,31 @@ app.get('/', (req, res) => {
     res.send("Hello, the server is up")
 })
 
-app.get('/API/:username/stockList', (req, res) => {
-
+app.get('/API/user/:username/stockList', (req, res) => {
+  res.status(200).json(fakeLastPrices)
 })
 
 app.get('/API/DowJones', (req, res) => {
-
+  res.status(200).json(indices.find( (symbol) => symbol.ticker === 'DJIA'))
 })
 
 app.get('/API/NASDAQ', (req, res) => {
-
+  res.status(200).json(indices.find( (symbol) => symbol.ticker === 'IXIC'))
 })
 
 app.get('/API/SandP500', (req, res) => {
-
+  res.status(200).json(indices.find( (symbol) => symbol.ticker === 'INX'))
 })
 
 app.get('/API/info/:symbol', (req, res) => {
-
+  const symb = req.params.symbol
+  const stockData = fakeLastPrices.find( (stock) => stock.ticker === symb)
+  if(stockData){
+    res.status(200).json(stockData)
+  }
+  else {
+    res.status(404).json('Could not get information for request')
+  }
 })
 
 app.post('/API/addStockToList', (req, res) => {
