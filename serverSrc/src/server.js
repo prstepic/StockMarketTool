2a19043bf45538a395c12a82c5aa788f3423ae7b
Preston Stepic <prestonstepic@Prestons-Air.attlocal.net>
2021-04-04 16:37:28 -0500

    server functions and fake data

--- serverSrc/src/server.js
+++ serverSrc/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const mongodb = require("mongodb")
+import { fakeLasPrices } from '../dummy-data'
+import { indices } from '../dummy-data'
 
 const app = express()
 const port = 9090
@@ -10,4 +12,36 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.send("Hello, the server is up")
+})
+
+app.get('/API/:username/stockList', (req, res) => {
+
+})
+
+app.get('/API/DowJones', (req, res) => {
+
+})
+
+app.get('/API/NASDAQ', (req, res) => {
+
+})
+
+app.get('/API/SandP500', (req, res) => {
+
+})
+
+app.get('/API/info/:symbol', (req, res) => {
+
+})
+
+app.post('/API/addStockToList', (req, res) => {
+
+})
+
+app.post('/API/addUser', (req, res) => {
+
+})
+
+app.delete('/API/removeStockFromList', (req, res) => {
+
 })
